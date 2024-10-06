7dc30b8b1a933a4007bea03a41dedb5b701f445a
Vladimir Rubin <vavakado@proton.me>
2024-10-06 21:03:49 +0300

    spicetify

--- configuration.nix
+++ configuration.nix
@@ -2,12 +2,13 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 
 {
   imports = [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ./modules/bundle.nix
+    inputs.spicetify-nix.nixosModules.default
   ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -72,6 +73,25 @@
     packages = [ ];
   };
 
+  programs.spicetify =
+    let spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+    in {
+      enable = true;
+      enabledExtensions = with spicePkgs.extensions; [
+        adblock
+        hidePodcasts
+        shuffle # shuffle+ (special characters are sanitized out of extension names)
+        fullAppDisplay
+        autoVolume
+        copyToClipboard
+        betterGenres
+        fullScreen
+        beautifulLyrics
+      ];
+      theme = spicePkgs.themes.text;
+      colorScheme = "base";
+    };
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
@@ -96,7 +116,7 @@
     fzf
     gamescope
     git
-		chezmoi
+    chezmoi
     flite
     vesktop
     qt6.qtwayland
@@ -141,7 +161,6 @@
     grim
     solaar
     soundconverter
-    spotify
     sqlite
     sshfs
     starship
--- flake.nix
+++ flake.nix
@@ -4,20 +4,24 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     zen-browser.url = "github:MarceColl/zen-browser-flake";
+    spicetify-nix = {
+      url = "github:Gerg-L/spicetify-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, zen-browser, ...}@inputs: 
-    let system = "x86_64-linux"; 
+  outputs = { self, nixpkgs, zen-browser, spicetify-nix, ... }@inputs:
+    let system = "x86_64-linux";
     in {
       nixosConfigurations = {
-uwuw = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = {
-          inherit self system;
-          inherit inputs;
-};
-modules = [ ./configuration.nix ];
-};
-};
+        uwuw = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = {
+            inherit self system;
+            inherit inputs;
+          };
+          modules = [ ./configuration.nix ];
+        };
+      };
     };
 }
