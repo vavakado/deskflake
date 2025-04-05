a95236ceb8673f60eb8cc5cddcebbb36426175e2
Vladimir Rubin <vavakado@proton.me>
2025-04-05 20:28:52 +0300

    issue 305891 was closed

--- modules/sunshine.nix
+++ modules/sunshine.nix
@@ -2,22 +2,9 @@
 {
   services.sunshine = {
     enable = true;
-    # Enable nvenc support (look for issue #305891)
-    package =
-      with pkgs;
-      (pkgs.sunshine.override {
-        cudaSupport = true;
-        cudaPackages = cudaPackages;
-      }).overrideAttrs
-        (old: {
-          nativeBuildInputs = old.nativeBuildInputs ++ [
-            cudaPackages.cuda_nvcc
-            (lib.getDev cudaPackages.cuda_cudart)
-          ];
-          cmakeFlags = old.cmakeFlags ++ [
-            "-DCMAKE_CUDA_COMPILER=${(lib.getExe cudaPackages.cuda_nvcc)}"
-          ];
-        });
+    package = pkgs.sunshine.override {
+      cudaSupport = true;
+    };
     openFirewall = true;
     capSysAdmin = true;
   };
