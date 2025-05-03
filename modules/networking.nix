6a2c4d56fed02c608dc1b502225cbd2c586af5bf
Vladimir Rubin <vavakado@proton.me>
2025-05-03 12:09:52 +0300

    fix: use quad9

--- modules/networking.nix
+++ modules/networking.nix
@@ -9,9 +9,18 @@
       userServices = true;
       workstation = true;
     };
+    openFirewall = true;
   };
 
   services.zerotierone.enable = true;
   networking.networkmanager.enable = true;
+  networking.nameservers = [
+    "9.9.9.9"
+    "149.112.112.112"
+  ];
+  services.dnsmasq.settings = {
+    port = 5353; # Change dnsmasq to use port 5353
+  };
+  services.resolved.extraConfig = "DNSStubListener=no";
   services.openssh.enable = true;
 }
